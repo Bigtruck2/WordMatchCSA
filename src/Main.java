326c29bb916f591c57fffd5afb0788cdf1eeecb7
Bigtruck2 <bigburrito363@gmail.com>
2025-02-26 11:29:31 -0500

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        WordMatch wordMatch = new WordMatch("concatenation");
+        System.out.println(wordMatch.scoreGuess("ten"));
+        System.out.println(wordMatch.scoreGuess("nation"));
+        System.out.println(wordMatch.findBetterGuess("ten" , "nation"));
+        System.out.println(wordMatch.scoreGuess("con"));
+        System.out.println(wordMatch.scoreGuess("cat"));
+        System.out.println(wordMatch.findBetterGuess("con" , "cat"));
     }
 }
